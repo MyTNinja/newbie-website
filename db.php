a25f394fc9c2c51e7f0493629eecf5cbe4c24879
Shourya Gupta <109819476+MyTNinja@users.noreply.github.com>
2023-04-26 18:29:35 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = 'localhost';
 $username = 'root';
-$password = <password>;
-$dbname = <dbname>;
+$password = 'password';
+$dbname = 'mydb';
 $conn = mysqli_connect($servername, $username, $password, "$dbname");
 if (!$conn) {
   die('Could not Connect MySql Server:' . mysql_error());
